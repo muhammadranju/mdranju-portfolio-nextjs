f54c940829a1d7dec7666ecdf2ed6c91ebdfe1cc
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-01-29 00:41:11 +0600

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        // pathname: "/nodelove/image/upload/f_auto,q_auto/v1/portfolio/",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
