d537e6551686f8fe6e3d67b77b0f1bdcd91cf219
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-01-31 10:06:40 +0600

    update Footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -4,28 +4,27 @@ let year = date.getFullYear();
 
 function Footer() {
   return (
-    <>
-      <footer className="w-full mt-10 p-2 ">
-        <hr className="my-4" />
-        <div className="mx-auto max-w-6xl items-center justify-between px-4 md:flex lg:px-0">
-          <Link href={"/"}>
-            <div className="inline-flex items-center">
-              <span className="bg-white rounded p-1 pr-0">
-                <svg
-                  width="30"
-                  height="30"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  version="1.0"
-                  viewBox="0 0 512.000000 512.000000"
+    <footer className="w-full mt-10 p-2 ">
+      <hr className="my-4" />
+      <div className="mx-auto max-w-6xl items-center justify-between px-4 md:flex lg:px-0">
+        <Link href={"/"}>
+          <div className="inline-flex items-center">
+            <span className="bg-white rounded p-1 pr-0">
+              <svg
+                width="30"
+                height="30"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.0"
+                viewBox="0 0 512.000000 512.000000"
+              >
+                <g
+                  transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                  fill="#000000"
+                  stroke="none"
                 >
-                  <g
-                    transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                    fill="#000000"
-                    stroke="none"
-                  >
-                    <path
-                      d="M2235 5104 c-591 -80 -1100 -334 -1510 -755 -267 -274 -430 -533
+                  <path
+                    d="M2235 5104 c-591 -80 -1100 -334 -1510 -755 -267 -274 -430 -533
 -582 -926 -3 -7 55 18 128 57 215 111 383 147 556 121 250 -39 369 -208 453
 -639 16 -83 60 -401 60 -434 0 -4 8 -8 18 -8 14 0 27 21 51 88 47 123 151 333
 212 427 246 378 528 562 874 568 93 2 116 -1 150 -18 49 -24 90 -99 101 -181
@@ -45,56 +44,55 @@ c25 -5 62 -24 83 -40 l38 -31 122 32 c156 41 364 52 492 26 205 -42 374 -172
 -276 -79 -497 -79 -159 -1 -207 3 -259 17 -189 54 -319 164 -372 315 -49 139
 -37 284 71 880 l68 377 -74 147 c-41 81 -74 150 -74 153 0 12 263 134 343 159
 123 40 247 54 327 35z"
-                    />
-                    <path
-                      d="M3860 3241 l-45 -6 -12 -75 c-7 -41 -51 -307 -97 -590 -110 -665
+                  />
+                  <path
+                    d="M3860 3241 l-45 -6 -12 -75 c-7 -41 -51 -307 -97 -590 -110 -665
 -106 -638 -89 -645 26 -9 210 -5 265 6 292 61 498 368 498 746 0 274 -114 484
 -295 542 -69 22 -159 31 -225 22z"
-                    />
-                    <path
-                      d="M101 3292 c-40 -137 -66 -265 -87 -422 -23 -175 -15 -554 14 -726 19
+                  />
+                  <path
+                    d="M101 3292 c-40 -137 -66 -265 -87 -422 -23 -175 -15 -554 14 -726 19
 -108 77 -353 86 -362 2 -2 2 44 -1 101 -9 161 14 331 149 1100 l22 127 -69
 141 c-39 77 -73 143 -76 145 -4 2 -21 -45 -38 -104z"
-                    />
-                  </g>
-                </svg>
-              </span>
+                  />
+                </g>
+              </svg>
+            </span>
 
-              <span className=" text-lg font-bold">R</span>
-            </div>
-          </Link>
-          <div className="mt-4 md:mt-0">
-            <p className="text-sm font-medium">
-              © {year}
-              <a href="https://github.com/muhammadranju" target="_blank">
-                <span className="font-bold text-indigo-500 hover:underline ml-1 mr-1">
-                  Muhammad Ranju.
-                </span>
-              </a>
-              All rights reserved.
-            </p>
-            <small>
-              Design by{" "}
-              <a
-                href="https://ui.shadcn.com/"
-                target="_blank"
-                className="hover:underline mr-1"
-              >
-                ui.shadcn
-              </a>
-              &
-              <a
-                href="https://www.devui.io/"
-                target="_blank"
-                className="hover:underline ml-1"
-              >
-                devui
-              </a>
-            </small>
+            <span className=" text-lg font-bold">R</span>
           </div>
+        </Link>
+        <div className="mt-4 md:mt-0">
+          <p className="text-sm font-medium">
+            © {year}
+            <a href="https://github.com/muhammadranju" target="_blank">
+              <span className="font-bold text-indigo-500 hover:underline ml-1 mr-1">
+                Muhammad Ranju.
+              </span>
+            </a>
+            All rights reserved.
+          </p>
+          <small>
+            Design by{" "}
+            <a
+              href="https://ui.shadcn.com/"
+              target="_blank"
+              className="hover:underline mr-1"
+            >
+              ui.shadcn
+            </a>
+            &
+            <a
+              href="https://www.devui.io/"
+              target="_blank"
+              className="hover:underline ml-1"
+            >
+              devui
+            </a>
+          </small>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 }
 
