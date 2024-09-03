9df2ea9d0d43eda9ec3811f8eb4ea4e81e2f7e74
Muhammad Ranju <mdranju23@gmail.com>
2024-09-04 00:21:26 +0600

    feat: added new style for display card items and update this section all card's look

--- src/ui/Projects.tsx
+++ src/ui/Projects.tsx
@@ -9,68 +9,73 @@ export async function Projects() {
   const post = result.slice(0, 8);
 
   return (
-    <div className=" w-full py-10 mb-5 flex flex-col items-center justify-center">
+    <div className="max-w-7xl mx-auto py-10 mb-5 flex flex-col items-center justify-center">
       {/* <hr className="my-6 w-full" /> */}
-      <div className="mx-auto w-auto container  rounded-full bg-gray-100 px-4 py-1.5">
+      <div className="mx-auto w-auto container  rounded-full bg-gray-200 dark:bg-slate-100 px-4 py-1.5">
         <p className="lg:text-lg text-sm font-extrabold uppercase tracking-widest text-slate-900">
           Hare are some of my projects I have done.
         </p>
       </div>
-      <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-9 md:space-y-0 lg:grid-cols-4">
+      <div className="grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:px-0 px-3  rounded-2xl lg:grid-cols-4">
         {post.map((post: any) => (
-          <div
-            key={post.title}
-            className="relative aspect-[16/9] shadow-lg  w-auto rounded-md md:aspect-auto md:h-[400px]"
-          >
+          <div key={post?.title} className="border rounded shadow-lg ">
             <Image
               src={post?.image}
+              className="aspect-video w-full rounded"
               width={500}
               height={500}
               blurDataURL="blur"
-              className="h-full  w-full rounded-md object-cover"
+              placeholder="blur"
               alt={post?.title}
             />
-
-            <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-800 to-transparent"></div>
-            <div className="absolute bottom-10 left-4 text-left">
-              <small className="text-white font-bold">
-                {post?.category.toLowerCase()}
-              </small>
-              <h1
-                className="text-lg font-medium text-white"
+            <div className="min-h-min p-3">
+              <p className="mt-4 w-full text-xs font-semibold leading-tight ">
+                #{post?.category.toLocaleLowerCase()}
+              </p>
+              <p
+                className="mt-4 flex-1 text-base font-semibold"
                 title={post?.title}
               >
-                {post?.title.length > 25
-                  ? post?.title.substring(0, 25).concat("...")
+                {post?.title.length > 40
+                  ? post?.title.substring(0, 50).concat("...")
                   : post?.title}
-              </h1>
-              <p className="mt-2 text-sm text-white" title={post?.details}>
-                {post.details.length > 25
-                  ? post?.details.substring(0, 65).concat("...")
+              </p>
+              <p
+                className="mt-2 w-full text-sm leading-normal "
+                title={post?.details}
+              >
+                {post?.details.length > 150
+                  ? post?.details.substring(0, 80).concat("...")
                   : post?.details}
               </p>
-              <Link href={post?.sourceCode} target="_blank">
-                <button className="mt-2 mr-5  inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-                  Source Code &rarr;
-                </button>
-              </Link>
-              {post?.liveLink ? (
-                <Link href={post?.liveLink} target="_blank">
-                  <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-                    Live Demo &rarr;
+              <div className="mt-4  flex justify-center items-center space-x-10 ">
+                <div>
+                  <button className="rounded-lg shadow-md px-3 py-2.5 text-sm font-semibold bg-slate-700 text-white hover:bg-indigo-600">
+                    <Link href={post?.sourceCode} target="_blank">
+                      View Code
+                    </Link>
                   </button>
-                </Link>
-              ) : (
-                ""
-              )}
+                </div>
+                {post?.liveLink ? (
+                  <div>
+                    <button className="rounded-lg shadow-md px-3 py-2.5 text-sm font-semibold bg-indigo-500 text-white hover:bg-indigo-600">
+                      <Link href={post?.liveLink} target="_blank">
+                        Live Demo
+                      </Link>
+                    </button>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         ))}
       </div>
-      <div className=" flex justify-center  text-center">
+      <div className=" flex justify-center items-center  text-center">
         <Link href={"/projects"}>
-          <button className="rounded-lg shadow-xl px-3 py-2.5 text-sm font-semibold bg-indigo-500 text-white hover:bg-indigo-600 ">
-            View More
+          <button className="rounded-lg shadow-xl px-3 py-2.5 text-base font-semibold bg-indigo-500 text-white hover:bg-indigo-600 ">
+            More Project
           </button>
         </Link>
       </div>
