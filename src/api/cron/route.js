d0af8fc77072b3b4e9ced3f9f316926c0a00b274
Muhammad Ranju <mdranju23@gmail.com>
2024-10-23 21:15:06 +0600

    Fix: Correct URL in cron job fetch request

--- src/api/cron/route.js
+++ src/api/cron/route.js
@@ -2,7 +2,7 @@ export const URL = "https://portfolio-project-api-sooty.vercel.app/v1/api";
 // const URL = "https://portfolio-project-api.onrender.com/v1/api/project";
 
 async function getProject() {
-  const data = await fetch(`${URL}/"project"`);
+  const data = await fetch(`${URL}/project`);
   const result = await data.json();
   return result;
 }
