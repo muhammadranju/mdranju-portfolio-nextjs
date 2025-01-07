4f699eef404ac675237b28c9a135b343dbd302be
Muhammad Ranju <mdranju23@gmail.com>
2025-01-07 12:12:17 +0600

    feat: Optimize ScrollToTop component by removing unnecessary searchParams dependency

--- src/utils/ScrollToTop/ScrollToTop.tsx
+++ src/utils/ScrollToTop/ScrollToTop.tsx
@@ -4,14 +4,13 @@ import { usePathname, useSearchParams } from "next/navigation";
 
 function ScrollToTop() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
